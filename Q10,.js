15c6ded8e13e7d23273b0b9309a4844a773525c8
Tayyab Hakroe <tayyabhakro2k21@gmail.com>
2025-02-28 09:08:22 -0800

    Q10 is solved

---
+++ Q10,.js
@@ -0,0 +1,19 @@
+//Write a function that will sort array with Bubble Sort
+
+function BubbleSort(arr){
+    for(let i = 0 ; i<arr.length -1 ; i++){
+        for(let j = 0 ; j<arr.length - i -1 ; j++){
+            if(arr[j] > arr[j+1]){
+                let tem = arr[j];
+                arr[j] = arr[j+1];
+              
+                arr[j+1] = tem
+            }
+        }
+    }
+    return arr
+
+}
+
+
+console.log(BubbleSort([10,43,65,45,2]))
