ba7dc4080427936e6c821e5092adcbb614ff5a8f
Tayyab Hakroe <tayyabhakro2k21@gmail.com>
2025-03-04 10:55:54 -0800

    building logic

--- Q11.js
+++ Q11.js
@@ -1,2 +1,12 @@
-let day = new Date()
-console.log(day.getDay())
+/**
+ * @param {string} jewels
+ * @param {string} stones
+ * @return {number}
+ */
+var numJewelsInStones = function(jewels, stones) {
+    let len = stones.length;
+    for (let i = 0; i < jewels.length; i++) stones = stones.replaceAll(jewels[i], "");
+    return len - stones.length;
+};
+
+console.log("aA" ,"aAsadsa")
